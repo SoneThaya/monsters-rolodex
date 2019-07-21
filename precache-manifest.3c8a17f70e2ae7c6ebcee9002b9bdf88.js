03ff7d94e812ade338170d6b5e6622c8bf03999c
Sone <sthaya@gmail.com>
2019-07-20 18:17:00 -0700

    Updates

---
+++ precache-manifest.3c8a17f70e2ae7c6ebcee9002b9bdf88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650f1ffe86183454f9b0da0e6b9d324a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "582e09432c800eb9a424",
+    "url": "/monsters-rolodex/static/css/main.7f89806f.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monsters-rolodex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "582e09432c800eb9a424",
+    "url": "/monsters-rolodex/static/js/main.2ecc6935.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
